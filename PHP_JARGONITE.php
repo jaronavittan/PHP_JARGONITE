8ca8a19a18904503b17ca0236208beeeb1e46fa3
jaronavittan <96601217+jaronavittan@users.noreply.github.com>
2023-03-14 10:37:16 -0500

    Add files via upload

---
+++ PHP_JARGONITE.php
@@ -0,0 +1,43 @@
+<?php
+$txt = "Glass Pain";
+echo "LIBRE/BrightGauge - $txt.";
+echo "Our team is currently working on this project. An update will be provided soon, but if you have any questions - please feel free to ask away."; // this prints
+
+
+//this is a comment. This is where you keep important notes, like this one - end lines of code (not variables) w/ a colomn (;).
+
+/* Multi line comments,
+look like this. */
+
+/*
+It call look like this,
+as well.
+*/
+
+ECHO "will print out something as well. But in order to deploy cleaner code, we'll need to keep the format unanimous.";
+
+$common = "This is one way of doing things.";
+$Common = "This is another.";
+$cOmmOn = "Heck, why not a third.";
+
+# this is another way to write a comment.
+
+$storage = "Store anything here, like a variable. Or an integer, like the one below.";
+$Storage = 3;
+
+echo strlen($Common); // This'll calculate the length of the string provided in the braces. Not entirely sure what it looks like in the end.
+
+$variables_can = "Look like that as well.";
+$4 = "This is also a variable apparently, but not entirely sure if it does the same thing as a regular one."
+
+// you can output variables like this within the echo just like in lines 2-3:
+
+$soursauce = "sour sauce!"
+echo = "i love" . $soursauce . "!";
+
+
+
+
+
+
+?>
